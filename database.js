bbd428f7eb13f3f8e504d497cdbd8f64e602cf4f
Asrafil041 <asrafil041@gmail.com>
2026-06-28 16:47:04 +0700

    feat: init sqlite database schema for users and laporan

---
+++ database.js
@@ -0,0 +1,41 @@
+const sqlite3 = require('sqlite3').verbose();
+const DBSOURCE = "campusfix.sqlite";
+
+let db = new sqlite3.Database(DBSOURCE, (err) => {
+    if (err) {
+        console.error(err.message);
+        throw err;
+    } else {
+        console.log('Terhubung ke database SQLite.');
+        
+        // Bikin tabel user (untuk US-01 Login)
+        db.run(`CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nama TEXT,
+            email TEXT UNIQUE,
+            password TEXT,
+            role TEXT
+        )`, (err) => {
+            if (!err) {
+                // Suntik 1 akun Admin default biar dosen bisa langsung ngetes login admin
+                const insert = 'INSERT OR IGNORE INTO users (id, nama, email, password, role) VALUES (?,?,?,?,?)';
+                db.run(insert, [1, "Admin Prasarana", "admin@unila.ac.id", "admin123", "admin"]);
+            }
+        });
+
+        // Bikin tabel laporan (untuk US-02 Form Kerusakan)
+        db.run(`CREATE TABLE IF NOT EXISTS laporan (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            kategori TEXT,
+            lokasi TEXT,
+            kronologi TEXT,
+            urgensi TEXT,
+            foto_bukti TEXT,
+            status TEXT DEFAULT 'Dilaporkan',
+            user_id INTEGER,
+            tanggal TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )`);
+    }
+});
+
+module.exports = db;
